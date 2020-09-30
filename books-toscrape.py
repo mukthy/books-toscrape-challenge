02c7082e8233a9aaa427521229ac1ad3ca292bc4
MUKTHEESWARAN MARIAPPAN <muktheeswaran.m@gmail.com>
2020-09-30 15:01:08 -0700

    First Commit Message

--- books-toscrape.py
+++ books-toscrape.py
@@ -15,14 +15,8 @@ class BooksToScrapeSpider(scrapy.Spider):
 
     def parse(self, response):
         all_books = response.xpath('.//article[@class="product_pod"]')
-
-# Loop to get all the categories books URL.
-
         for book in all_books:
             book_url = book.xpath('.//h3/a/@href').extract_first()
-
-# To check if the Catalogue is present in the URL or not, if it is not present then it will add it in the URL.
-
             if 'catalogue/' not in book_url:
                 book_url = 'catalogue/' + book_url
 
@@ -33,8 +27,6 @@ class BooksToScrapeSpider(scrapy.Spider):
             partial_image_url = book.xpath('.//div[@class="image_container"]/a/img/@src').extract_first()
             image_url = self.base_url + partial_image_url.replace('../','')
 
-# Expoting the Data as per the requirement
-
             yield {
                 'Title': title,
                 'Price': price,
@@ -42,12 +34,7 @@ class BooksToScrapeSpider(scrapy.Spider):
                 'Book URL': book_url,
             }
 
-# Next Page URL extraction
-
         next_page_url = response.xpath('//li[@class="next"]/a/@href').extract_first()
-
-# First page url is having catalogue, but from second page URL the Catalogue is missing in URL. So adding Catalogue in URL it is not present.
-
         if next_page_url:
             if 'catalogue/' not in next_page_url:
                 next_page_url = "catalogue/" + next_page_url
